eea708d9199ad120f31eb749098b73b311304576
MariaST98 <143325493+MariaST98@users.noreply.github.com>
2023-08-27 16:30:27 +0300

    WeatherApp

---
+++ WeatherApp/app/src/main/java/com/example/weatherapp/adapters/VpAdapter.kt
@@ -0,0 +1,15 @@
+package com.example.weatherapp.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class VpAdapter(fa:FragmentActivity, private val List: List<Fragment>) :FragmentStateAdapter(fa){
+    override fun getItemCount(): Int {
+         return  List.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+         return List[position]
+    }
+}
